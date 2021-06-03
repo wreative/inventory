41e3b99943a29bad3164858eb02d6779f802cf63
rdp77 <ravidwiputra@gmail.com>
2021-06-03 15:14:00 +0700

    Update Assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,15 +16,17 @@ mix.styles(
     [
         "resources/css/nprogress.css",
         "resources/css/mfb.min.css",
-        "resources/css/select2.min.css",
-        "resources/css/select2-bootstrap.min.css",
         "resources/css/bootstrap.min.css",
         "resources/css/daterangepicker.css",
         "resources/css/dataTables.bootstrap4.min.css",
         "resources/css/searchBuilder.dataTables.min.css",
         "resources/css/buttons.bootstrap4.min.css",
         "resources/css/responsive.bootstrap4.min.css",
+        "resources/css/select2.min.css",
+        "resources/css/chocolat.css",
+        "resources/css/bootstrap-tagsinput.css",
         "resources/css/style.css",
+        "resources/css/summernote-bs4.css",
         "resources/css/components.css",
     ],
     "public/assets.css"
@@ -34,14 +36,14 @@ mix.styles(
 mix.scripts(
     [
         "resources/js/nprogress.js",
+        "resources/js/mfb.min.js",
+        "resources/js/daterangepicker.js",
         "resources/js/bootstrap.min.js",
         "resources/js/stisla.js",
-        "resources/js/daterangepicker.js",
         "resources/js/jquery.dataTables.min.js",
         "resources/js/dataTables.bootstrap4.min.js",
         "resources/js/dataTables.searchBuilder.min.js",
         "resources/js/dataTables.buttons.min.js",
-        "resources/js/jszip.min.js",
         "resources/js/buttons.bootstrap4.min.js",
         "resources/js/buttons.flash.min.js",
         "resources/js/buttons.html5.min.js",
@@ -50,10 +52,15 @@ mix.scripts(
         "resources/js/dataTables.responsive.min.js",
         "resources/js/responsive.bootstrap4.min.js",
         "resources/js/Chart.min.js",
+        "resources/js/bootstrap-tagsinput.js",
         "resources/js/select2.full.min.js",
         "resources/js/cleave.min.js",
         "resources/js/cleave-phone.id.js",
+        "resources/js/jquery.chocolat.js",
         "resources/js/scripts.js",
+        "resources/js/summernote-bs4.js",
     ],
     "public/assets.js"
 ).version();
+
+mix.disableNotifications();
