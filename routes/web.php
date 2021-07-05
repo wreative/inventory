e4ed4e43a3a4e4ccbdfe4a88d80187536105b018
rdp77 <ravidwiputra@gmail.com>
2021-07-05 10:26:00 +0700

    Update Route

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
     ->name('home');
 
